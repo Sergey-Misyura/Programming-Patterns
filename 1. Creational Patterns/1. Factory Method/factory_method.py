788c3101aa387570af7677a41ee769558b8cf661
Sergey Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2023-01-08 19:31:34 +0300

    Update Factory Method

--- 1. Creational Patterns/1. Factory Method/factory_method.py
+++ 1. Creational Patterns/1. Factory Method/factory_method.py
@@ -56,3 +56,15 @@ if __name__ == "__main__":
 
     print("App: Launched with the ConcreteCreator2.")
     client_code(ConcreteCreator2())
+
+'''Output:
+App: Launched with the ConcreteCreator1.
+Client: I'm not aware of the creator's class, but it still works.
+Creator: The same creator's code has just worked with {Result of the ConcreteProduct1}
+
+App: Launched with the ConcreteCreator2.
+Client: I'm not aware of the creator's class, but it still works.
+Creator: The same creator's code has just worked with {Result of the ConcreteProduct2}
+'''
+
+
